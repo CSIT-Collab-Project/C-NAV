5c0dd27927e6c940635bd1067840c10329820941
Tavi Pollard <tavienpollard@gmail.com>
2022-01-21 13:16:35 -0500

    frontend displays directions

--- Frontend/scripts.js
+++ Frontend/scripts.js
@@ -1,17 +1,18 @@
-const directionList = document.getElementById("location-submit").addEventListener("click", () => {
+document.getElementById("location-submit").addEventListener("click", getDirections);
+
+async function getDirections() {
     const currentRoom = document.getElementById("initial-location").value;
     const destination = document.getElementById("destination").value;
-    const directions = requestDirections (currentRoom, destination);
+    const directions = await requestDirections (currentRoom, destination);
     const directionDisplay = document.getElementById("direction-display");
     directionDisplay.innerHTML = directions;
+    return directions;
 }
-);
 
 
 async function requestDirections (start, end) {
     const url = `/directions-${start}-${end}`;
-    let directionsJson = await fetch(url);
-    console.log(directionsJson);
-    directionsJson = "hello world";
+    let directionsJson = await fetch(url)
+        .then(response => response.json());
     return directionsJson;
 }
