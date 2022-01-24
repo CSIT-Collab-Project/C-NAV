667dcbc683333ec33ad78581a24700dd125ed8e3
Tavi Pollard <tavienpollard@gmail.com>
2022-01-24 13:27:33 -0500

    start interactive ui

--- Frontend/scripts.js
+++ Frontend/scripts.js
@@ -3,16 +3,25 @@ document.getElementById("location-submit").addEventListener("click", getDirectio
 async function getDirections() {
     const currentRoom = document.getElementById("initial-location").value;
     const destination = document.getElementById("destination").value;
-    const directions = await requestDirections (currentRoom, destination);
+    const directions = await requestDirections(currentRoom, destination);
     const directionDisplay = document.getElementById("direction-display");
     directionDisplay.innerHTML = directions;
     return directions;
 }
 
 
-async function requestDirections (start, end) {
+async function requestDirections(start, end) {
     const url = `/directions-${start}-${end}`;
     let directionsJson = await fetch(url)
         .then(response => response.json());
     return directionsJson;
 }
+
+const destinationForm = document.getElementById("destination-form");
+const nextBtn = document.getElementById("next");
+destinationForm.style.display = "none";
+nextBtn.addEventListener("click", () => {
+    destinationForm.style.display = "block";
+    nextBtn.style.display = "none";
+});
+
