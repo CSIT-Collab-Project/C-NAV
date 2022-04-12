aa65cbf528ccbf7649dd9edc8e30a64565d739dd
TWBrew <twbrewitt@gmail.com>
2022-04-12 12:31:36 -0400

    Made changes for production server

--- Backend/main.py
+++ Backend/main.py
@@ -3,73 +3,62 @@ import path
 import sys
 from flask import Flask
 from flask.helpers import send_from_directory
-from Backend.run import main
+from run import main
 
 sys.path.append(path.Path(__file__).abspath().parent.parent)
 from Backend.Logger.logger import logger
 
-app = Flask(__name__)
-app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
+def create_app():
+    app = Flask(__name__)
+    app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
-@app.route('/login')
-def login():
-    logger.info('login')
-    return send_from_directory('../Frontend', 'login.html')
+    @app.route('/login')
+    def login():
+        logger.info('login')
+        return send_from_directory('../Frontend', 'login.html')
 
+    @app.route('/about')
+    def about():
+        logger.info('about')
+        return send_from_directory('../Frontend', 'about.html')
 
-@app.route('/about')
-def about():
-    logger.info('about')
-    return send_from_directory('../Frontend', 'about.html')
+    @app.route('/')
+    def home():
+        logger.info('home')
+        return send_from_directory('../Frontend', 'index.html')
 
+    @app.route('/style.css')
+    def css():
+        logger.info('css')
+        return send_from_directory('../Frontend', 'style.css')
 
-@app.route('/')
-def home():
-    logger.info('home')
-    return send_from_directory('../Frontend', 'index.html')
+    @app.route('/scripts.js')
+    def js():
+        logger.info('js')
+        return send_from_directory('../Frontend', 'scripts.js')
 
+    @app.route('/icon-<iconname>')
+    def icons(iconname):
+        logger.info('icons')
+        return send_from_directory('../Frontend/Icons', iconname + ".png")
 
-@app.route('/style.css')
-def css():
-    logger.info('css')
-    return send_from_directory('../Frontend', 'style.css')
+    @app.route('/directions-<start>-<end>')
+    async def directions(start, end):
+        logger.info('directions')
+        directionjson = await main(start, end)
+        print(directionjson)
+        return directionjson
 
+    @app.route('/map<floor>')
+    def getMap(floor):
+        logger.info('getmap')
+        return send_from_directory('../Backend', f'map_path{int(floor) - 1}.png')
 
-@app.route('/scripts.js')
-def js():
-    logger.info('js')
-    return send_from_directory('../Frontend', 'scripts.js')
-
-
-@app.route('/icon-<iconname>')
-def icons(iconname):
-    logger.info('icons')
-    return send_from_directory('../Frontend/Icons', iconname + ".png")
-
-
-@app.route('/directions-<start>-<end>')
-async def directions(start, end):
-    logger.info('directions')
-    directionjson = await main(start, end)
-    print(directionjson)
-    return directionjson
-
-
-@app.route('/map<floor>')
-def getMap(floor):
-    logger.info('getmap')
-    return send_from_directory('../Backend', f'map_path{int(floor) - 1}.png')
-
-def run():
-    logger.info('run')
-    app.run(port=8080, debug=True)
-
-def run():
-    logger.info('run')
-    app.run(port=8080, debug=True)
+    return app
 
 
 if __name__ == '__main__':
     logger.info('run')
-    app.run(port=8080, debug=True)
+    app = create_app()
+    app.run(host="localhost", port=8080, debug=True)
--- wsgi.py
+++ wsgi.py
@@ -5,4 +5,4 @@ import flask
 app = flask.Flask(__name__)
 
 if __name__ == "__main__":
-    main.run()
+    main
