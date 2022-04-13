c96daec706aa41539bbf044e266c0cc29332e5ab
TWBrew <twbrewitt@gmail.com>
2022-04-13 13:03:44 -0400

    Now gets port rather than forcing

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+import os
+
 import server
 from waitress import serve
 
 if __name__ == '__main__':
-    print('Running on port 80')
-    serve(server.create_app(), host="localhost", port=80)
+    PORT = os.environ.get('PORT', 80)
+
+    print(f'Running on port {PORT}')
+    serve(server.create_app(), host="localhost", port=PORT)
