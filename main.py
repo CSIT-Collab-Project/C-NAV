a3e3279912c06ea48d90ae586c071354d9e5bcf4
TWBrew <twbrewitt@gmail.com>
2022-04-13 12:46:51 -0400

    Runs on python file

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ import server
 from waitress import serve
 
 if __name__ == '__main__':
-    print('Running on port 8080')
-    serve(server.create_app(), host="localhost", port=8080)
+    print('Running on port 80')
+    serve(server.create_app(), host="localhost", port=80)
